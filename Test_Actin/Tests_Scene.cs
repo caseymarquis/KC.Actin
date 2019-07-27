e53fb8b871b2fd7d89f2b63de7c343480e9d0556
Casey <caseymarquis@gmail.com>
2019-07-27 11:26:27 -0400

    Rename/Reorganize test classes.

---
+++ Test_Actin/Tests_Scene.cs
@@ -0,0 +1,66 @@
+using KC.Actin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test.Actin
+{
+    public class SceneTests
+    {
+        [Singleton]
+        public class TheScene : Scene<SomeInstanceType> {
+            protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 10);
+
+            public MessageQueue<int> RunningIds = new MessageQueue<int>();
+
+            protected override async Task<IEnumerable<Role>> CastActors(ActorUtil util, Dictionary<int, SomeInstanceType> myActors) {
+                return await Task.FromResult(new Role[] {
+                new Role {
+                    Id = 1,
+                },
+                new Role {
+                    Id = 2,
+                },
+            });
+            }
+        }
+
+        [Instance]
+        public class SomeInstanceType : Actor {
+            protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 10);
+
+            [FlexibleParent]
+            private TheScene theScene;
+
+            protected async override Task OnRun(ActorUtil util) {
+                theScene.RunningIds.Enqueue(this.Id);
+                await Task.FromResult(0);
+                return;
+            }
+        }
+
+        [Fact]
+        public async Task RunManualAndDIProcs()
+        {
+            var director = new Director();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            director.Run(startUp_loopUntilSucceeds: false, startUp: async (util) => {
+                var nestedTypes = typeof(SceneTests).GetNestedTypes();
+                util.FilterRootActors(x => nestedTypes.Contains(x.Type));
+                await Task.FromResult(0);
+            }, assembliesToCheckForDI: Assembly.GetExecutingAssembly());
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            await Task.Delay(250);
+            var sceneData = director.GetSingleton<TheScene>();
+            if (!sceneData.RunningIds.TryDequeueAll(out var ids)) {
+                throw new Exception("No actors were run in the scene.");
+            }
+            Assert.Contains(1, ids);
+            Assert.Contains(2, ids);
+        }
+    }
+}
--- Test_Actin/Tests_Singleton.cs
+++ Test_Actin/Tests_Singleton.cs
@@ -1,53 +1,56 @@
 using KC.Actin;
 using System;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Test.Actin
 {
-    [Instance]
-    public class Poco {
-    }
-
-    public class ProcManual : Actor {
-        protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 10);
-
-        public MessageQueue<int> SelfRan = new MessageQueue<int>();
-        public MessageQueue<int> DiRan = new MessageQueue<int>();
-
-        [Instance]
-        public Poco AutoPoco;
-        public Poco ManualPocoLeaveNull;
-
-        protected async override Task OnRun(ActorUtil util) {
-            SelfRan.Enqueue(0);
-            await Task.FromResult(0);
-        }
-    }
-
-    [Singleton]
-    public class ProcDI : Actor{
-        [Singleton]
-        ProcManual procManual;
-
-        protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 10);
-        protected async override Task OnRun(ActorUtil util) {
-            procManual.DiRan.Enqueue(1);
-            await Task.FromResult(0);
-            return;
-        }
-    }
-
     public class SingletonTests
     {
+        [Instance]
+        public class Poco {
+        }
+
+        public class ProcManual : Actor {
+            protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 10);
+
+            public MessageQueue<int> SelfRan = new MessageQueue<int>();
+            public MessageQueue<int> DiRan = new MessageQueue<int>();
+
+            [Instance]
+            public Poco AutoPoco;
+            public Poco ManualPocoLeaveNull;
+
+            protected async override Task OnRun(ActorUtil util) {
+                SelfRan.Enqueue(0);
+                await Task.FromResult(0);
+            }
+        }
+
+        [Singleton]
+        public class ProcDI : Actor {
+            [Singleton]
+            ProcManual procManual;
+
+            protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 10);
+            protected async override Task OnRun(ActorUtil util) {
+                procManual.DiRan.Enqueue(1);
+                await Task.FromResult(0);
+                return;
+            }
+        }
+
         [Fact]
-        public async Task RunManualAndDIProcs()
+        public async Task BuildAndRunASingleton()
         {
             var director = new Director();
             var procManual = new ProcManual();
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             director.Run(startUp_loopUntilSucceeds: false,startUp: async (util) => {
+                var nestedTypes = typeof(SingletonTests).GetNestedTypes();
+                util.FilterRootActors(x => nestedTypes.Contains(x.Type));
                 director.AddSingletonDependency(procManual);
                 await Task.FromResult(0);
             }, assembliesToCheckForDI: Assembly.GetExecutingAssembly());
