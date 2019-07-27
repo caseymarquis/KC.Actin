3abcd62eeeb319b7c987e41bdc972f5e0879ec15
Casey <caseymarquis@gmail.com>
2019-07-27 15:39:29 -0400

    Test: Circular dependency detection.

---
+++ Test_Actin/Test_CircularDependencyDetection.cs
@@ -0,0 +1,41 @@
+using KC.Actin;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test.Actin
+{
+    public class Tests_CircularDependencyDetection
+    {
+        [Instance]
+        public class ClassA {
+            [Instance]
+            private ClassB B;
+        }
+
+        public class ClassB {
+            [Instance]
+            private ClassC C;
+        }
+
+        public class ClassC {
+            [Instance]
+            private ClassA A;
+        }
+
+        [Fact]
+        public async Task ThrowOnCircularDependency()
+        {
+            var director = new Director();
+            await Assert.ThrowsAsync<ApplicationException>(async () => {
+                await director.Run(startUp_loopUntilSucceeds: false,startUp: async (util) => {
+                    var nestedTypes = typeof(Tests_CircularDependencyDetection).GetNestedTypes();
+                    util.FilterRootActors(x => nestedTypes.Contains(x.Type));
+                    await Task.FromResult(0);
+                }, assembliesToCheckForDI: Assembly.GetExecutingAssembly());
+            });
+        }
+    }
+}
