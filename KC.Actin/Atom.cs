99c68fce2999d08e1b588b571e3ff55e0d1f1fea
Casey <caseymarquis@gmail.com>
2019-07-27 23:19:13 -0400

    Locking primitive: Atom

---
+++ KC.Actin/Atom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KC.Actin {
+    public class Atom<T> {
+        public Atom() {
+
+        }
+
+        public Atom(T value) {
+            m_value = value;
+        }
+
+        private object lockValue = new object();
+        private T m_value;
+        public T Value {
+            get {
+                lock (lockValue) {
+                    return m_value;
+                }
+            }
+            set {
+                lock (lockValue) {
+                    m_value = value;
+                }
+            }
+        }
+    }
+}
