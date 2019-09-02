9ac372503238d39abd36598dea84fbacfcb80d55
Casey <caseymarquis@gmail.com>
2019-09-02 14:59:27 -0400

    Tests: Parents and FlexibleParents

---
+++ Test_Actin/Tests_FlexibleParent.cs
@@ -0,0 +1,193 @@
+using KC.Actin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test.Actin {
+    public class FlexibleParentTests {
+        public interface IHaveParent {
+            IHaveParent Parent { get; set; }
+        }
+
+        [Singleton]
+        public class AllDependencies {
+            public MessageQueue<IHaveParent> Dependencies = new MessageQueue<IHaveParent>();
+        }
+
+        public abstract class PocoWithParent : IOnInit, IHaveParent {
+            [FlexibleParent]
+            public IHaveParent Parent { get; set; }
+
+            [Singleton]
+            AllDependencies disposedList;
+
+            public async Task OnInit(ActorUtil util) {
+                disposedList.Dependencies.Enqueue(this);
+                await Task.FromResult(0);
+            }
+        }
+
+        [Singleton]
+        public class SingletonPocoRoot : PocoWithParent {
+            [Instance]
+            InstancePocoChild child;
+            [Instance]
+            InstanceActorChild actorChild;
+        }
+
+        [Instance]
+        public class InstancePocoChild : PocoWithParent {
+        }
+
+        [Instance]
+        public class InstanceActorChild : Actor, IHaveParent {
+            [FlexibleParent]
+            public IHaveParent Parent { get; set; }
+
+            [Singleton]
+            AllDependencies allDependencies;
+
+            [Instance]
+            InstancePocoChild child;
+
+            protected override async Task OnRun(ActorUtil util) {
+                await Task.FromResult(0);
+            }
+
+            protected override async Task OnInit(ActorUtil util) {
+                allDependencies.Dependencies.Enqueue(this);
+                await Task.FromResult(0);
+            }
+        }
+
+        [Singleton]
+        public class SingletonActor : Actor, IHaveParent {
+            [FlexibleParent]
+            public IHaveParent Parent { get; set; }
+
+            [Singleton]
+            AllDependencies allDependencies;
+
+            [Instance]
+            InstanceActorChild actorChild;
+            [Instance]
+            InstancePocoChild child;
+
+            protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 10);
+            protected async override Task OnRun(ActorUtil util) {
+                await Task.FromResult(0);
+            }
+
+            protected override async Task OnInit(ActorUtil util) {
+                allDependencies.Dependencies.Enqueue(this);
+                await Task.FromResult(0);
+            }
+        }
+
+        [Singleton]
+        public class SingletonScene : Scene<ActorInScene>, IHaveParent {
+            [FlexibleParent]
+            public IHaveParent Parent { get; set; }
+
+            [Singleton]
+            AllDependencies allDependencies;
+
+            [Instance]
+            InstanceActorChild actorChild;
+            [Instance]
+            InstancePocoChild child;
+
+            protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 10);
+            protected override async Task<IEnumerable<Role>> CastActors(ActorUtil util, Dictionary<int, ActorInScene> myActors) {
+                return await Task.FromResult(new Role[] {
+                    new Role {
+                        Id = 1,
+                    },
+                });
+            }
+
+            protected override async Task OnInit(ActorUtil util) {
+                allDependencies.Dependencies.Enqueue(this);
+                await Task.FromResult(0);
+            }
+        }
+
+        [Instance]
+        public class ActorInScene : Actor, IHaveParent {
+            [FlexibleParent]
+            public IHaveParent Parent { get; set; }
+
+            [Singleton]
+            AllDependencies allDependencies;
+
+            [Instance]
+            InstanceActorChild actorChild;
+            [Instance]
+            InstancePocoChild child;
+
+            protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 10);
+            protected async override Task OnRun(ActorUtil util) {
+                await Task.FromResult(0);
+            }
+
+            protected override async Task OnInit(ActorUtil util) {
+                allDependencies.Dependencies.Enqueue(this);
+                await Task.FromResult(0);
+            }
+        }
+
+        [Fact]
+        public async Task TestFlexibleParents() {
+            var director = new Director();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            director.Run(startUp_loopUntilSucceeds: false, startUp: async (util) => {
+                var nestedTypes = typeof(FlexibleParentTests).GetNestedTypes();
+                util.FilterRootActors(x => nestedTypes.Contains(x.Type));
+                await Task.FromResult(0);
+            }, assembliesToCheckForDI: Assembly.GetExecutingAssembly());
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            await Task.Delay(250);
+            director.Dispose();
+            await Task.Delay(250);
+
+            var allDependencies = director.GetSingleton<AllDependencies>().Dependencies.DequeueAll();
+
+            //SingletonPocoRoot
+            //  InstancePocoChild
+            //  InstanceActorChild
+            //      InstancePocoChild
+            Assert.Single(allDependencies.Where(x => x is SingletonPocoRoot));
+            Assert.Single(allDependencies.Where(x => x is InstancePocoChild && x.Parent is SingletonPocoRoot));
+            Assert.Single(allDependencies.Where(x => x is InstanceActorChild && x.Parent is SingletonPocoRoot)); Assert.Single(allDependencies.Where(x => x is InstancePocoChild && x.Parent is InstanceActorChild && x.Parent.Parent is SingletonPocoRoot));
+
+            //SingletonActor
+            //  InstancePocoChild
+            //  InstanceActorChild
+            //      InstancePocoChild
+            Assert.Single(allDependencies.Where(x => x is SingletonActor));
+            Assert.Single(allDependencies.Where(x => x is InstancePocoChild && x.Parent is SingletonActor));
+            Assert.Single(allDependencies.Where(x => x is InstanceActorChild && x.Parent is SingletonActor));
+            Assert.Single(allDependencies.Where(x => x is InstancePocoChild && x.Parent is InstanceActorChild && x.Parent.Parent is SingletonActor));
+
+            //SingletonScene
+            //  InstancePocoChild
+            //  InstanceActorChild
+            //      InstancePocoChild
+            Assert.Single(allDependencies.Where(x => x is SingletonScene));
+            Assert.Single(allDependencies.Where(x => x is InstancePocoChild && x.Parent is SingletonScene));
+            Assert.Single(allDependencies.Where(x => x is InstanceActorChild && x.Parent is SingletonScene));
+            Assert.Single(allDependencies.Where(x => x is InstancePocoChild && x.Parent is InstanceActorChild && x.Parent.Parent is SingletonScene));
+            //  ActorInScene
+            //    InstancePocoChild
+            //    InstanceActorChild
+            //        InstancePocoChild
+            Assert.Single(allDependencies.Where(x => x is ActorInScene));
+            Assert.Single(allDependencies.Where(x => x is InstancePocoChild && x.Parent is ActorInScene));
+            Assert.Single(allDependencies.Where(x => x is InstanceActorChild && x.Parent is ActorInScene));
+            Assert.Single(allDependencies.Where(x => x is InstancePocoChild && x.Parent is InstanceActorChild && x.Parent.Parent is ActorInScene));
+        }
+    }
+}
---
+++ Test_Actin/Tests_Parent.cs
@@ -0,0 +1,135 @@
+using KC.Actin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test.Actin {
+    public class ParentTests {
+        public interface IHasParent {
+            IHasParent Parent { get; }
+        }
+
+        [Singleton]
+        public class AllInjectedDependencies {
+            public MessageQueue<IHasParent> InjectedDependencies = new MessageQueue<IHasParent>();
+        }
+
+        public abstract class InjectedPoco : IOnInit, IHasParent {
+            [Singleton]
+            AllInjectedDependencies allDependencies;
+            public abstract IHasParent Parent { get; }
+
+            public async Task OnInit(ActorUtil util) {
+                allDependencies.InjectedDependencies.Enqueue(this);
+                await Task.FromResult(0);
+            }
+        }
+
+        public abstract class ActorChild : Actor, IHasParent {
+            [Singleton]
+            AllInjectedDependencies allDependencies;
+
+            public abstract IHasParent Parent { get; }
+
+            protected override async Task OnInit(ActorUtil util) {
+                allDependencies.InjectedDependencies.Enqueue(this);
+                await Task.FromResult(0);
+            }
+
+            protected override async Task OnRun(ActorUtil util) {
+                await Task.FromResult(0);
+            }
+        }
+
+        [Singleton]
+        public class SingletonPoco : InjectedPoco {
+            [Instance]
+            SingletonPocoPocoChild child;
+            [Instance]
+            SingletonPocoActorChild actorChild;
+
+            public override IHasParent Parent => null;
+        }
+
+        [Instance]
+        public class SingletonPocoPocoChild : InjectedPoco {
+            [Parent]
+            private SingletonPoco parent; 
+            public override IHasParent Parent => parent;
+        }
+
+        [Instance]
+        public class SingletonPocoActorChild : ActorChild {
+            [Parent]
+            private SingletonPoco parent; 
+            public override IHasParent Parent => parent;
+        }
+
+        [Singleton]
+        public class SingletonActor : Actor, IHasParent {
+            public IHasParent Parent => null;
+
+            [Singleton]
+            AllInjectedDependencies allDependencies;
+
+            [Instance]
+            SingletonActorPocoChild child;
+            [Instance]
+            SingletonActorActorChild actorChild;
+
+            protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 10);
+
+            protected override async Task OnInit(ActorUtil util) {
+                allDependencies.InjectedDependencies.Enqueue(this);
+                await Task.FromResult(0);
+            }
+
+            protected async override Task OnRun(ActorUtil util) {
+                await Task.FromResult(0);
+            }
+
+        }
+
+        [Instance]
+        public class SingletonActorPocoChild : InjectedPoco {
+            [Parent]
+            private SingletonActor parent;
+            public override IHasParent Parent => parent;
+        }
+
+        [Instance]
+        public class SingletonActorActorChild : ActorChild {
+            [Parent]
+            private SingletonActor parent;
+            public override IHasParent Parent => parent;
+        }
+
+        [Fact]
+        public async Task TestParents() {
+            var director = new Director();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            director.Run(startUp_loopUntilSucceeds: false, startUp: async (util) => {
+                var nestedTypes = typeof(ParentTests).GetNestedTypes();
+                util.FilterRootActors(x => nestedTypes.Contains(x.Type));
+                await Task.FromResult(0);
+            }, assembliesToCheckForDI: Assembly.GetExecutingAssembly());
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            await Task.Delay(250);
+            director.Dispose();
+            await Task.Delay(250);
+
+            var allDependencies = director.GetSingleton<AllInjectedDependencies>().InjectedDependencies.DequeueAll();
+
+            Assert.Single(allDependencies.Where(x => x is SingletonPoco));
+            Assert.Single(allDependencies.Where(x => x is SingletonPocoPocoChild && x.Parent is SingletonPoco));
+            Assert.Single(allDependencies.Where(x => x is SingletonPocoActorChild && x.Parent is SingletonPoco));
+
+            Assert.Single(allDependencies.Where(x => x is SingletonActor));
+            Assert.Single(allDependencies.Where(x => x is SingletonActorPocoChild));
+            Assert.Single(allDependencies.Where(x => x is SingletonActorActorChild));
+        }
+    }
+}
