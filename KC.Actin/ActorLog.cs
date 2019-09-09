1c225cdd9ead9b591d9362f7cd63603199f8002d
Casey Marquis <caseymarquis@gmail.com>
2019-09-09 15:52:56 -0400

    Minor: Future proof not yet implemented function.

--- KC.Actin/ActorLog.cs
+++ KC.Actin/ActorLog.cs
@@ -5,6 +5,7 @@ using System.Text;
 namespace KC.Actin {
     public class ActorLog : IActinLogger {
         public void Log(ActinLog log) {
+            log = log.WithNoNulls();
             //TODO: Store X number of logs...
         }
     }
