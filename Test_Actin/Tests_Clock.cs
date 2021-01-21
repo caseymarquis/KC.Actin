4094c835444716a9b5d67dfe5126fdc553d4c44c
Casey Marquis <caseymarquis@gmail.com>
2021-01-21 14:20:39 -0500

    Tests IgnoreSimulatedTime

--- Test_Actin/Tests_Clock.cs
+++ Test_Actin/Tests_Clock.cs
@@ -35,6 +35,40 @@ namespace Test.Actin
             }
         }
 
+        [Singleton]
+        public class DoIgnoreSimulatedTime : Actor {
+            public DoIgnoreSimulatedTime(){
+                this.IgnoreSimulatedTime = true;
+            }
+
+            protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 200);
+
+            public Atom<int> TimesRunAtom = new Atom<int>();
+
+            protected async override Task OnRun(ActorUtil util) {
+                TimesRunAtom.Value += 1;
+                await Task.FromResult(0);
+                return;
+            }
+        }
+
+        [Singleton]
+        public class DoNotIgnoreSimulatedTime : Actor {
+            public DoNotIgnoreSimulatedTime() {
+                this.IgnoreSimulatedTime = false;
+            }
+
+            protected override TimeSpan RunDelay => new TimeSpan(0, 0, 0, 0, 200);
+
+            public Atom<int> TimesRunAtom = new Atom<int>();
+
+            protected async override Task OnRun(ActorUtil util) {
+                TimesRunAtom.Value += 1;
+                await Task.FromResult(0);
+                return;
+            }
+        }
+
         [Fact]
         public async Task ManuallyStepThroughTime()
         {
@@ -100,6 +134,37 @@ namespace Test.Actin
             //We don't expect it to be exact, as the Director has a finite resolution, but it should be in the ballpark:
             Assert.True(5*normalTimes < spedUpTimes);
             Assert.True(15*normalTimes > spedUpTimes);
+        }
+
+        [Fact]
+        public async Task IgnoreSimulatedTime() {
+            var director = new Director();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            director.Run(configure: async (util) => {
+                var nestedTypes = typeof(ClockTests).GetNestedTypes();
+                util.Set_RootActorFilter(x => nestedTypes.Contains(x.Type));
+                util.Set_AssembliesToCheckForDependencies(Assembly.GetExecutingAssembly());
+                await Task.FromResult(0);
+            });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            await Task.Delay(100);
+            var normal = director.GetSingleton<DoIgnoreSimulatedTime>();
+            var spedUp = director.GetSingleton<DoNotIgnoreSimulatedTime>();
+
+            //Reset to a baseline:
+            normal.TimesRunAtom.Value = 0;
+            spedUp.TimesRunAtom.Value = 0;
+
+            var clock = director.Clock;
+            clock.Simulate(null, 10);
+
+            await Task.Delay(3000);
+            var normalTimes = normal.TimesRunAtom.Value;
+            var spedUpTimes = spedUp.TimesRunAtom.Value;
+
+            //We don't expect it to be exact, as the Director has a finite resolution, but it should be in the ballpark:
+            Assert.True(5 * normalTimes < spedUpTimes);
+            Assert.True(15 * normalTimes > spedUpTimes);
         }
 
     }
